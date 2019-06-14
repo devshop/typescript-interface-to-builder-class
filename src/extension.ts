b0998815682908d463c9d8a2152073bb30d267fb
Fraser Crosbie <frasercrosbie@gmail.com>
2019-06-13 21:38:17 -0600

    Code cleanup
    Added editorconfig & prettier. Started moving code into method.

--- src/extension.ts
+++ src/extension.ts
@@ -1,101 +1,191 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from 'fs'
+import * as path from 'path'
+// tslint:disable-next-line: no-implicit-dependencies
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'extension.interfaceToBuilder',
+    () => {
+      const uppercaseFirstLetter = (s: string) =>
+        s.charAt(0).toUpperCase() + s.slice(1)
 
-	let disposable = vscode.commands.registerCommand('extension.interfaceToBuilder', () => {
+      const verifyWorkspaceLoaded = () => {
+        if (!vscode.workspace.workspaceFolders) {
+          vscode.window.showErrorMessage(
+            'Please open a directory before creating a builder.'
+          )
+          return false
+        }
+        return true
+      }
 
-		const uppercaseFirstLetter = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+      const verifyTextEditorOpen = () => {
+        if (!vscode.window.activeTextEditor) {
+          vscode.window.showErrorMessage(
+            'No open text editor. Please open an interface file.'
+          )
+          return false
+        }
+        return true
+      }
 
-		const start = async () => {
+      const verifyTextInEditor = (text: string | null) => {
+        if (!text) {
+          vscode.window.showErrorMessage(
+            'No text found. Please open an interface file.'
+          )
+          return false
+        }
+        return true
+      }
 
-			if (!vscode.workspace.workspaceFolders) {
-				return vscode.window.showErrorMessage('Please open a directory before creating a builder.');
-			}
+      const verifyInterfaceInText = (text: string) => {
+        if (!text.includes('export interface')) {
+          vscode.window.showErrorMessage(
+            'No interface found. "export interface" must be in your code.'
+          )
+          return false
+        }
+        return true
+      }
 
-			var editor = vscode.window.activeTextEditor;
-			
-			if (!editor) {
-				vscode.window.showErrorMessage('No open text editor. Please open an interface file.');
-				return;
-			}
+      const getInterfaceName = (text: string) => {
+        // Search for the first word after "export interface" to find the name of the interface.
+        const interfaceNames = text.match(/(?<=\bexport interface\s)(\w+)/)
+        if (!interfaceNames) {
+          vscode.window.showErrorMessage('Could not find the interface name.')
+          return null
+        }
+        return interfaceNames[0]
+      }
 
-			var text = editor.document.getText();
+      const getClassName = (text: string) => {
+        // Check if the interface name has an "I" at the start. If it does, remove the the "I".
+        if (/\b[I]/.test(text)) {
+          return text.substring(1)
+        }
+        return text
+      }
 
-			if (!text) {
-				vscode.window.showErrorMessage('No text found. Please open an interface file.');
-				return;
-			}
+      const getInterfaceProperties = (text: string) => {
+        // Find all the properties defined in the interface by looking for words before a colon(:)
+        const properties = text.match(/(\w*[^\s])(?=:)/gm)
+        if (!properties) {
+          vscode.window.showErrorMessage(
+            'Could not find any properties defined in the interface.'
+          )
+          return null
+        }
+        return properties
+      }
 
-			if (!text.includes('export interface')) {
-				vscode.window.showErrorMessage('No interface found. "export interface" must be in your code.');
-				return;
-			}	
+      const getInterfacePropertyTypes = (text: string) => {
+        // Find all the property types defined in the interface by looking for words after a colon(:)
+        const types = text.match(/(?<=:\s)(.*)/g)
+        if (!types) {
+          vscode.window.showErrorMessage(
+            'Could not find any property types defined in the interface.'
+          )
+          return null
+        }
+        return types
+      }
 
-			// Search for the first word after "export interface" to find the name of the interface.
-			const interfaceNames = text.match(/(?<=\bexport interface\s)(\w+)/);
+      const saveBuilderFile = (editor: vscode.TextEditor, text: string) => {
+        const filePath = editor!.document.uri.fsPath
+        const fileName = filePath.match(/[a-z.-]+(?=\.ts)/)![0]
+        let folderPath = filePath.substring(0, filePath.lastIndexOf('/'))
 
-			if (!interfaceNames) {
-				vscode.window.showErrorMessage('Could not find the interface name.');
-				return;
-			}
+        if (!folderPath) {
+          folderPath = filePath.substring(0, filePath.lastIndexOf('\\'))
+        }
 
-			let interfaceName = interfaceNames[0];
-			let className = interfaceName;
+        // Write the file to the current editor directory.
+        fs.writeFile(
+          path.join(folderPath, `${fileName}.builder.ts`),
+          text,
+          err => {
+            if (err) {
+              vscode.window.showErrorMessage(`File save failed: ${err}`)
+              return
+            }
+          }
+        )
 
-			// Check if the interface name has an "I" at the start. If it does, remove the the "I".
-			if (/\b[I]/.test(className)) {
-				className = className.substring(1);
-			}
+        vscode.window.showInformationMessage(
+          `Builder class saved to: ${path.join(
+            folderPath,
+            `${fileName}.builder.ts`
+          )}`
+        )
+      }
 
-			// Find all the properties defined in the interface.
-			const properties = text.match(/(\w*[^\s])(?=:)/gm);
+      const getInitalPropertyValue = (type: string) => {
+        switch (type) {
+          case 'string[]':
+            return `[undefined]`
+          case 'number':
+            return 1
+          default:
+            return undefined
+        }
+      }
 
-			if (!properties) {
-				vscode.window.showErrorMessage('Could not find any properties defined in the interface.');
-				return;
-			}
+      const start = async () => {
+        const isWorkspaceLoaded = verifyWorkspaceLoaded()
+        if (!isWorkspaceLoaded) {
+          return
+        }
 
-			// Find all the property types defined in the interface.
-			const types = text.match(/(?<=:\s)(.*)/g);
+        const isEditorOpen = verifyTextEditorOpen()
+        if (!isEditorOpen) {
+          return
+        }
 
-			if (!types) {
-				vscode.window.showErrorMessage('Could not find any property types defined in the interface.');
-				return;
-			}
-			
-			const propertyDefinitions: string[] = [];
-			const propertyLocalAssignments: string[] = [];
-			const propertyExternalAssignments: string[] = [];
+        const editor = vscode.window.activeTextEditor
+        const text = editor!.document.getText()
 
-			properties.forEach((p, i) => {
-				const type = types[i];
-				let initValue;
+        const isTextInEditor = verifyTextInEditor(text)
+        if (!isTextInEditor) {
+          return
+        }
 
-				switch (type) {
-					case 'string': 
-						initValue = `''`;
-						break;
-					case 'number': 
-						initValue = 1;
-						break;
-					default:
-						initValue = `''`;
-				}
+        const isInterfaceInText = verifyInterfaceInText(text)
+        if (!isInterfaceInText) {
+          return
+        }
 
-				const propertyDefinition = `private ${p}: ${type} = ${initValue}`;
-				propertyDefinitions.push(propertyDefinition);
+        const interfaceName = getInterfaceName(text)
+        if (!interfaceName) {
+          return
+        }
 
-				const propertyLocalAssignment = `${p}: this.${p}`;
-				propertyLocalAssignments.push(propertyLocalAssignment);
+        const properties = getInterfaceProperties(text)
+        const types = getInterfacePropertyTypes(text)
+        if (!properties || !types) {
+          return
+        }
 
-				const propertyExternalAssignment = `public with${uppercaseFirstLetter(p)}(value: ${type}) {\r\n    this.${p} = value\r\n    return this\r\n  }`;
-				propertyExternalAssignments.push(propertyExternalAssignment);
-			});
+        const propertyDefinitions: string[] = []
+        const propertyLocalAssignments: string[] = []
+        const propertyExternalAssignments: string[] = []
 
-			// TODO: Make the indenting prettier.
-			const classString = `export class ${className}Builder {
+        properties.forEach((p, i) => {
+          const type = types[i]
+          const value = getInitalPropertyValue(type)
+          propertyDefinitions.push(`private ${p}: ${type} = ${value}`)
+          propertyLocalAssignments.push(`${p}: this.${p}`)
+          const propertyExternalAssignment = `public with${uppercaseFirstLetter(
+            p
+          )}(value: ${type}) {\r\n    this.${p} = value\r\n    return this\r\n  }`
+          propertyExternalAssignments.push(propertyExternalAssignment)
+        })
+
+        // TODO: Make the indenting prettier.
+        const classString = `export class ${getClassName(
+          interfaceName
+        )}Builder {
   ${propertyDefinitions.join('\r\n  ')}
 
   public build(): ${interfaceName} {
@@ -105,34 +195,14 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   ${propertyExternalAssignments.join('\r\n\r\n  ')}
-}\r\n`;
+}\r\n`
 
-			const filePath = editor.document.uri.fsPath;
-			const fileName = filePath.match(/[a-z.-]+(?=\.ts)/)![0];
-			let folderPath = filePath.substring(0, filePath.lastIndexOf('/'));
+        saveBuilderFile(editor!, classString)
+      }
 
-			if (!folderPath) {
-				folderPath = filePath.substring(0, filePath.lastIndexOf('\\'));
-			}
+      start()
+    }
+  )
 
-			// Write the file to the current editor directory.
-			fs.writeFile(path.join(folderPath, `${fileName}.builder.ts`),
-			classString, 
-			err => {
-				if (err) {
-					console.log(err);
-					vscode.window.showErrorMessage("File save failed");
-					return;
-				}
-			});
-
-			vscode.window.showInformationMessage(`Builder class saved to: ${path.join(folderPath, `${fileName}.builder.ts`)}`);
-		};
-
-		start();		
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
-
-export function deactivate() {}
